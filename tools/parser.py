3604c071a7d33160abf460c2e3ccd6a05501b7ed
R. D. Poor <rdpoor@gmail.com>
2022-10-13 19:40:43 -0700

    Make descriptors const

--- tools/parser.py
+++ tools/parser.py
@@ -36,10 +36,17 @@ class JrpcDescriptor(object):
         return self._ffi
 
     def struct_names(self):
+        """
+        Iterate over the list of the ffi typedefs, return only those that
+        define structs.
+        """
         typedefs = self._ffi.list_types()[0]  # all typedefs
         return [x for x in typedefs if self._ffi.typeof(x).kind == 'struct']
 
     def message_name(self, typedef_name):
+        """
+        Convert `foo_t` to `foo`
+        """
         return typedef_name.removesuffix('_t')
 
     def dump_descriptor_typedefs(self):
@@ -51,22 +58,23 @@ typedef struct {
 
 typedef struct {
     const char *name;
-    slot_descriptor_t slots[];
+    const slot_descriptor_t slots[]; // flexible array members (null terminated)
 } message_descriptor_t;
 """
 
     def dump_descriptors(self):
-        return ''.join([self.dump_descriptor(name) for name in self.struct_names()])
+        struct_names = self.struct_names()
+        return ''.join([self.dump_descriptor(name) for name in struct_names])
 
     def dump_descriptor(self, typedef_name):
         message_name = self.message_name(typedef_name)
         return f"""
-static message_descriptor_t s_{message_name} = {{
+static const message_descriptor_t s_{message_name} = {{
     .name="{message_name}",
     .slots=
         {self.dump_slot_descriptors(typedef_name)},
         NULL
-    }},
+    }}
 }};
 """
 
@@ -74,27 +82,6 @@ static message_descriptor_t s_{message_name} = {{
         t = ffi.typeof(typedef_name)
         slots = [f'{{.name="{field_name}", .type="{field.type.cname}"}}' for field_name, field in t.fields]
         return ',\n        '.join(slots)
-
-# def dumper(my_type_string):
-#     mytype = ffi.typeof(my_type_string)
-#     mytypedict = {}
-#     for field_name, field in mytype.fields:
-#         mytypedict[field_name] = field.type.cname
-#
-#     print(json.dumps(mytypedict))
-
-def dumper(my_type_string):
-    mytype = ffi.typeof(my_type_string)
-    a = [f'{{.name="{field_name}", .type="{field.type.cname}"}}' for field_name, field in mytype.fields]
-    print(a)
-
-dumper('button_state_t')
-dumper('light_sensor_t')
-
-x0 = ffi.typeof("timestamp_t")    # x0.kind = 'primitive'
-x1 = ffi.typeof("button_state_t") # x1.kind = 'struct'
-x2 = ffi.typeof("color_t")        # x2.kind = 'enum'
-x3 = ffi.typeof("light_sensor_t") # x3.kind = 'struct'
 
 j = JrpcDescriptor(ffi)
 print(j.dump_descriptor_typedefs())
